259742e0b949874410997aeca9cc078656482226
George Royce <george.royce@detroitlabs.com>
2018-05-09 20:12:10 -0400

    refactor url creation and data model parsing into separate methods

--- wunderground-practica-2/Services/WeatherService.swift
+++ wunderground-practica-2/Services/WeatherService.swift
@@ -16,28 +16,37 @@ class WeatherService {
     
     func getCurrentConditions(completion: @escaping (CurrentWeatherConditions) -> Void) {
         
-        var components = URLComponents()
-        components.scheme = weatherUndergroundScheme
-        components.host = weatherUndergroundHost
-        components.path = "/api/\(weatherUndergroundKey)/conditions/q/42.3314,-83.0458.json"
-        
-        guard let url = components.url else {
+        guard let url = urlForCurrentConditions() else {
             print("could not create url for current conditions")
             return
         }
         
         NetworkManager.getDataFor(url: url) { (data) in
-            //parse data into models
             do {
-                let rawWeatherInfo = try JSONDecoder().decode(IntermediateRawCurrentConditions.self, from: data)
-                let rawCurrentConditions = rawWeatherInfo.current_observation
-                let currentConditions = CurrentWeatherConditions(rawCurrentConditions)
-                
+                let currentConditions = try self.parseCurrentConditionsFrom(data)
                 completion(currentConditions)
+                
             } catch {
-                print("Weather Service error: \(error)")
+                print("could not parse response into current conditions models: \(error)")
             }
         }
+    }
+    
+    private func urlForCurrentConditions() -> URL? {
+        var components = URLComponents()
+        components.scheme = weatherUndergroundScheme
+        components.host = weatherUndergroundHost
+        components.path = "/api/\(weatherUndergroundKey)/conditions/q/42.3314,-83.0458.json"
+        
+        return components.url
+    }
+    
+    private func parseCurrentConditionsFrom(_ data: Data) throws -> CurrentWeatherConditions {
+        let rawWeatherInfo = try JSONDecoder().decode(IntermediateRawCurrentConditions.self, from: data)
+        let rawCurrentConditions = rawWeatherInfo.current_observation
+        let currentConditions = CurrentWeatherConditions(rawCurrentConditions)
+        
+        return currentConditions
     }
 }
 
